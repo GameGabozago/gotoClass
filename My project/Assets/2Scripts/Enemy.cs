4cf42b97efe5dada88d15d9c48b21fbf73b40a12
Minsu Kim <qmdlrhdfyd@naver.com>
2022-11-20 19:20:05 +0900

    implement grenade explosion

--- My project/Assets/2Scripts/Enemy.cs
+++ My project/Assets/2Scripts/Enemy.cs
@@ -24,7 +24,7 @@ public class Enemy : MonoBehaviour
             Weapon Weapon = other.GetComponent<Weapon>();
             curHealth -= Weapon.damage;
             Vector3 reactVec = transform.position - other.transform.position;
-            StartCoroutine(OnDamage(reactVec));
+            StartCoroutine(OnDamage(reactVec, false));
 
         }
         else if (other.tag == "Bullet"){
@@ -32,11 +32,19 @@ public class Enemy : MonoBehaviour
             curHealth -= bullet.damage;
             Vector3 reactVec = transform.position - other.transform.position;
             Destroy(other.gameObject);
-            StartCoroutine(OnDamage(reactVec));
+            StartCoroutine(OnDamage(reactVec, false));
         }
     }
 
-    IEnumerator OnDamage(Vector3 reactVec)
+    public void HitByGrenade(Vector3 explosionPos)
+    {
+        curHealth -= 100;
+        Vector3 reactVec = transform.position - explosionPos;
+        StartCoroutine(OnDamage(reactVec, true));
+        
+    }
+
+    IEnumerator OnDamage(Vector3 reactVec, bool isGrenade)
     {
         mat.color = Color.red;
         yield return new WaitForSeconds(0.1f);
@@ -47,10 +55,21 @@ public class Enemy : MonoBehaviour
         else{
             mat.color = Color.gray;
             gameObject.layer = 14;
-            reactVec = reactVec.normalized;
-            reactVec += Vector3.up;
-            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
-            Destroy(gameObject, 4);
+
+            if (isGrenade){
+                reactVec = reactVec.normalized;
+                reactVec += Vector3.up * 3;
+
+                rigid.freezeRotation = false;
+                rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+                rigid.AddTorque(reactVec * 15, ForceMode.Impulse);
+            }
+            else{
+                reactVec = reactVec.normalized;
+                reactVec += Vector3.up;
+                rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+            }
+                Destroy(gameObject, 4);
         }
 
     }
