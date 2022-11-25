cfbde5732360137eecfa0aae81e7aff12b7c261f
rodom <enjoying1018@naver.com>
2022-11-25 12:04:26 +0900

    test monster + monstera(tracking)

--- My project/Assets/2Scripts/Enemy.cs
+++ My project/Assets/2Scripts/Enemy.cs
@@ -1,37 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
+    public Transform target;
+    public bool isChase;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
+    Material mat;
+    NavMeshAgent nav;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        Invoke("ChaseStart", 2);
     }
 
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+    void Update()
+    {
+        if (isChase)
+        {
+            nav.SetDestination(target.position);
+        }
+    }
+
+    void FreezeVelocity()
+    {
+        if (isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Melee")
         {
             Weapon weapon = other.GetComponent<Weapon>();
             curHealth -= weapon.damage;
+            Vector3 reactVec = transform.position - other.transform.position;
+            Destroy(other.gameObject);
 
-            Debug.Log("Melee : " + curHealth);
+            StartCoroutine(OnDamage(reactVec));
+
+            //Debug.Log("Melee : " + curHealth);
 
         }
         else if (other.tag == "Bullet")
         {
             Bullet bullet = other.GetComponent<Bullet>();
             curHealth -= bullet.damage;
+            Vector3 reactVec = transform.position - other.transform.position;
+            Destroy(other.gameObject);
 
-            Debug.Log("Range : " + curHealth);
+            StartCoroutine(OnDamage(reactVec));
+
+            //Debug.Log("Range : " + curHealth);
+        }
+    }
+
+    IEnumerator OnDamage(Vector3 reactVec)
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if(curHealth > 0)
+        {
+            mat.color = Color.white;
+        }
+        else
+        {
+            mat.color = Color.gray;
+            gameObject.layer = 14;
+            isChase = false;
+            nav.enabled = false;
+            anim.SetTrigger("doDie");
+
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+
+            Destroy(gameObject, 4);
         }
     }
 }
