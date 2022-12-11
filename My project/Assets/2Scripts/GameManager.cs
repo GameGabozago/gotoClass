c64382ba2b3bf5070f6685444a3ffb5e7792582d
yoon-bang <tlol91@naver.com>
2022-12-11 22:31:24 +0900

    stage

--- My project/Assets/2Scripts/GameManager.cs
+++ My project/Assets/2Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,12 +12,22 @@ public class GameManager : MonoBehaviour
     public GameObject gameCam;
     public Player player;
     public Boss boss;
-    public int stage;
+
+    public GameObject itemShop;
+	public GameObject weaponShop;
+	public GameObject startZone;
+
+	public int stage;
     public float playTime;
     public bool isBattle;
     public int enemyCntA;
     public int enemyCntB;
     public int enemyCntC;
+    public int enemyCntD;
+
+    public Transform[] enemyZones;
+    public GameObject[] enemies;
+    public List<int> enemyList;
 
     public GameObject menuPanel;
     public GameObject gamePanel;
@@ -46,6 +58,8 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        enemyList = new List<int>();
+        
         maxScoreTxt.text = string.Format("{0:n0}", PlayerPrefs.GetInt("MaxScore"));
 
         titleBGM.Play();
@@ -63,6 +77,97 @@ public class GameManager : MonoBehaviour
 
         titleBGM.Stop();
         storeBGM.Play();
+    }
+
+    public void StageStart()
+    {
+        itemShop.SetActive(false);
+        weaponShop.SetActive(false);
+        startZone.SetActive(false);
+
+        foreach (Transform zone in enemyZones)
+            zone.gameObject.SetActive(true);
+
+
+
+        isBattle = true;
+        StartCoroutine(InBattle());
+    }
+
+    public void StageEnd()
+    {
+        //이 부분이 에러나서 주석처리해둡니다. 플레이어 위치 초기화
+        player.transform.position = new Vector3(0, 1.24f, -5);
+
+		itemShop.SetActive(true);
+		weaponShop.SetActive(true);
+		startZone.SetActive(true);
+
+		foreach (Transform zone in enemyZones)
+			zone.gameObject.SetActive(false);
+
+		isBattle = false;
+        stage++;
+    }
+
+    IEnumerator InBattle()
+    {
+		//스테이지가 5단위일때 보스 소환
+		if (stage % 5 == 0)
+		{
+            enemyCntD++;
+			GameObject instantEnemy = Instantiate(enemies[3], enemyZones[0].position, enemyZones[0].rotation);
+			Enemy enemy = instantEnemy.GetComponent<Enemy>();
+			enemy.target = player.transform;
+            enemy.manager = this;
+            boss = instantEnemy.GetComponent<Boss>();
+		}
+		else
+		{
+			for (int index = 0; index < stage; index++)
+			{
+
+				int ran = UnityEngine.Random.Range(0, 3);
+				enemyList.Add(ran);
+
+				switch (ran)
+				{
+					case 0:
+						enemyCntA++;
+						break;
+					case 1:
+						enemyCntB++;
+						break;
+					case 2:
+						enemyCntC++;
+						break;
+				}
+			}
+
+			while (enemyList.Count > 0)
+			{
+				int ranZone = UnityEngine.Random.Range(0, 4);
+				GameObject instantEnemy = Instantiate(enemies[enemyList[0]], enemyZones[ranZone].position, enemyZones[ranZone].rotation);
+				Enemy enemy = instantEnemy.GetComponent<Enemy>();
+				enemy.target = player.transform;
+                enemy.manager = this;
+				enemyList.RemoveAt(0);
+				yield return new WaitForSeconds(4f);
+			}
+		}
+        //yield return new WaitForSeconds(5);
+        //StageEnd();
+
+        while (enemyCntA + enemyCntB + enemyCntC + enemyCntD > 0) //아직 못잡은 몬스터 남아있으면 while문
+        {
+            yield return null;
+        }
+
+        //몬스터 다 잡으면 스테이지 종료
+        yield return new WaitForSeconds(4f);
+
+        boss = null;
+        StageEnd();
     }
 
     void Update()
@@ -99,6 +204,16 @@ public class GameManager : MonoBehaviour
         enemyBTxt.text = enemyCntB.ToString();
         enemyCTxt.text = enemyCntC.ToString();
 
-        bossHealthBar.localScale = new Vector3((float)boss.curHealth / (float)boss.maxHealth, 1 ,1);
+
+        if(boss != null)
+        {
+            bossHealthGroup.anchoredPosition = Vector3.down * 30;
+			bossHealthBar.localScale = new Vector3((float)boss.curHealth / (float)boss.maxHealth, 1, 1);
+		}
+        else
+        {
+			bossHealthGroup.anchoredPosition = Vector3.up * 200;
+		}
+            
     }
 }
